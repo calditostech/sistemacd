f151070189fdf24097acd93eb48763721c8fb158
Ricardo Moreira <calditos1988@outlook.com>
2021-04-21 13:12:35 -0300

    criando as telas de login

---
+++ login-php/dump.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE login;
+USE login;
+
+CREATE TABLE `login`.`usuario` (
+  `usuario_id` INT NOT NULL AUTO_INCREMENT,
+  `usuario` VARCHAR(200) NOT NULL,
+  `senha` VARCHAR(32) NOT NULL,
+  PRIMARY KEY (`usuario_id`));
+
+INSERT INTO `usuario` (`usuario_id`,`usuario`,`senha`) VALUES (1,'ricardo','e10adc3949ba59abbe56e057f20f883e');
